7dd835df92e3b4795e731bb2d8ebc809dbb12342
kkaiocarvalho <kaioscsouza@gmail.com>
2024-10-23 15:00:33 -0300

    feat: new styles for theme switch mode

--- front-end/src/components/mode-toggle.tsx
+++ front-end/src/components/mode-toggle.tsx
@@ -1,38 +1,30 @@
 import { Moon, Sun } from "lucide-react"
-
-import { Button } from "@/components/ui/button"
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
-import { useTheme } from "@/components/theme-provider"
 import { Switch } from "@/components/ui/switch"
-import { set } from "react-hook-form"
-import { useState } from "react"
-
+import { useTheme } from "@/components/theme-provider"
 
 export function ModeToggle() {
-  const { setTheme, theme } = useTheme() // Define the 'theme' variable
-  const [isOn, setIsOn] = useState(false)
+  const { setTheme, theme } = useTheme()
 
   const handleSwitchChange = (checked: boolean) => {
-    setIsOn(checked)
-    if (checked) {
-      setTheme("dark")
-    } else {
-      setTheme("light")
-    }
+    setTheme(checked ? "dark" : "light")
   }
 
   return (
-<div className="flex items-center space-x-2">
-  <Switch id="theme-mode" checked={isOn} onCheckedChange={handleSwitchChange} />
-  <div>
-  {theme === "dark" ?  <Moon className=" h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />: <Sun className= " h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />}
-  </div>
-</div>
+    <div className="flex items-center space-x-2">
+      <Switch 
+        id="theme-mode" 
+        checked={theme === "dark"}
+        onCheckedChange={handleSwitchChange}
+        className="data-[state=checked]:bg-gray-500 data-[state=unchecked]:bg-yellow-500"
+      />
+      <div>
+        {theme === "dark" ? (
+          <Moon className="h-[1.2rem] w-[1.2rem] text-gray-500" />
+        ) : (
+          <Sun className="h-[1.2rem] w-[1.2rem] text-yellow-500" />
+        )}
+      </div>
+    </div>
     /* <DropdownMenu>
       <DropdownMenuTrigger asChild>
       <Button variant="outline" size="icon">
