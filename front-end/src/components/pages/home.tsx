bddcb8fe48cbf5a99f057f5cfed64ece2b08cf93
Wendel Junior <wendel_junior@hotmail.com>
2024-10-23 15:32:00 -0300

    feat: HomeScreen styles

--- front-end/src/components/pages/home.tsx
+++ front-end/src/components/pages/home.tsx
@@ -1,6 +1,7 @@
 import { useTheme } from '@/components/theme-provider'
-
 import { Tabs, TabsList, TabsTrigger, TabsContent } from "@/components/ui/tabs";
+import { Input } from '../ui/input';
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '../ui/card';
 
 export default function HomePage() {
     const { theme } = useTheme();
@@ -12,11 +13,11 @@ export default function HomePage() {
         "Leonardo"
     ];
 
-    const search = [
-        "Slipknot",
-        "É ele",
-        "Minha vida"
-    ];
+    // const search = [
+    //     "Slipknot",
+    //     "É ele",
+    //     "Minha vida"
+    // ];
     
     const playlists = [
         "Gospel",
@@ -34,16 +35,43 @@ export default function HomePage() {
           <TabsTrigger value="playlist" className='w-full'>Playlists</TabsTrigger>
         </TabsList>
         <TabsContent value="top20">
+            <Card>
+                <CardContent>
+                    <CardHeader>
+                        <CardTitle className='text-center'>Top 20 Músicas</CardTitle>
+                    </CardHeader>
+                    <CardDescription>
             {/* biome-ignore lint/suspicious/noArrayIndexKey: <explanation> */}
-            {top20.map((item, index) => (<div key={index}>{item}</div>))}
+            {top20.map((item, index) => (<div className='items-center justify-center flex' key={index}>{item}</div>))}
+                    </CardDescription>
+                </CardContent>
+            </Card>
+
         </TabsContent>
         <TabsContent value="pesquisa">
-            {/* biome-ignore lint/suspicious/noArrayIndexKey: <explanation> */}
-            {search.map((item, index) => (<div key={index}>{item}</div>))}
+            <Card>
+                <CardContent>
+                    <CardHeader>
+                        <CardTitle className='text-center'>Buscar Música</CardTitle>
+                    </CardHeader>
+                    <CardDescription className='flex justify-center items-center px-10'>
+                        <Input type="text" placeholder="Buscar música" className='w-full'/>
+                    </CardDescription>
+                </CardContent>
+            </Card>
         </TabsContent>
         <TabsContent value="playlist">
-            {/* biome-ignore lint/suspicious/noArrayIndexKey: <explanation> */}
-            {playlists.map((item, index) => (<div key={index}>{item}</div>))}
+            <Card>
+                <CardContent>
+                    <CardHeader>
+                        <CardTitle>Playlists</CardTitle>
+                    </CardHeader>
+                    <CardDescription>
+                    {/* biome-ignore lint/suspicious/noArrayIndexKey: <explanation> */}
+                    {playlists.map((item, index) => (<div key={index}>{item}</div>))}
+                    </CardDescription>
+                </CardContent>
+            </Card>
         </TabsContent>
       </Tabs>
     </div>
